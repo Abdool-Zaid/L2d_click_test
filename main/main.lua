92f4c6f217816bc7cde25860d83b805094bda7bd
Abdool-Zaid <aztoorabally7447@gmail.com>
2023-09-06 16:38:04 +0200

    first commit

---
+++ main/main.lua
@@ -0,0 +1,13 @@
+local ran = require("ran_gen")
+function love.draw()
+    local width = love.graphics.getWidth()
+    local height = love.graphics.getHeight()
+    local tar = {}
+        tar.size=20
+        tar.color= ran.color_RGB()
+        tar.x= math.random(width)
+        tar.y=math.random(height)
+love.graphics.setColor(tar.color.r,tar.color.g,tar.color.b)
+love.graphics.print(tar.color.r.." ".. tar.color.g.. " ".. tar.color.b, 1,1)
+love.graphics.circle("fill",tar.x,tar.y,tar.size)
+end
