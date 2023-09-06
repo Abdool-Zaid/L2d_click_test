b720652e471a54cd6ab48775acf5c6b4a27cf42e
Abdool-Zaid <aztoorabally7447@gmail.com>
2023-09-06 21:52:05 +0200

    fin

--- main/main.lua
+++ main/main.lua
@@ -1,13 +1,5 @@
 local ran = require("ran_gen")
 function love.draw()
-    local width = love.graphics.getWidth()
-    local height = love.graphics.getHeight()
-    local tar = {}
-        tar.size=20
-        tar.color= ran.color_RGB()
-        tar.x= math.random(width)
-        tar.y=math.random(height)
-love.graphics.setColor(tar.color.r,tar.color.g,tar.color.b)
-love.graphics.print(tar.color.r.." ".. tar.color.g.. " ".. tar.color.b, 1,1)
-love.graphics.circle("fill",tar.x,tar.y,tar.size)
+local col = ran.color_RGB()
+love.graphics.setBackgroundColor(col.r,col.g,col.b,1    )
 end
