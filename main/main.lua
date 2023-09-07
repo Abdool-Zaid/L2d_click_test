c7d6c77d3326c71d9884f8a81baee6dcfa47638a
Abdool-Zaid <aztoorabally7447@gmail.com>
2023-09-07 19:47:14 +0200

    'movement'

--- main/main.lua
+++ main/main.lua
@@ -7,14 +7,14 @@ local dt
 function love.load ()
     dt = love.timer.getTime()
     tar.size=20
-    tar.color= ran.color_RGB()
     local width = love.graphics.getWidth()
     local height = love.graphics.getHeight()
+    tar.color= ran.color_RGB()
     tar.x= math.random(width)
     tar.y=math.random(height)
-    end
+end
 
-    local clickX, clickY = -1, -1
+local clickX, clickY = -1, -1
 
 function love.mousepressed(x, y, button, istouch, presses)
     -- Check if the left mouse button was pressed (button 1)
@@ -26,20 +26,27 @@ end
 
 
 function love.update(dt)
-
     
-        if clickX >= 0 and clickY >= 0 then
-            local hit = logic.check_pos(tar.x,tar.y,tar.size,clickX,clickY)
-            if hit == true then
+    
+    if clickX >= 0 and clickY >= 0 then
+        local hit = logic.check_pos(tar.x,tar.y,tar.size,clickX,clickY)
+        if hit == true then
             score   = score + 1
             clickX, clickY = -1
-            end 
-        end
+            local width = love.graphics.getWidth()
+            local height = love.graphics.getHeight()
+            tar.color= ran.color_RGB()
+            tar.x= math.random(width)
+            tar.y=math.random(height)
+            
+        end 
+    end
 
 end    
         
 function love.draw()
     local dur= love.timer.getTime() - dt
+    love.graphics.setColor(1,1,1)
     love.graphics.print("score: ".. score)
     love.graphics.setColor(tar.color.r,tar.color.g,tar.color.b)
     love.graphics.circle("fill",tar.x,tar.y,tar.size)
